3224584011f8bbabab6d136e0650aefa8847ead5
Riccardo <ric.menon2000@gmail.com>
2020-08-15 20:16:23 +1000

    refactored coal generator

--- Assets/Scripts/Controllers/Generator/CoalGeneratorController.cs
+++ Assets/Scripts/Controllers/Generator/CoalGeneratorController.cs
@@ -2,57 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoalGeneratorController : MonoBehaviour, IInteractable {
+public class CoalGeneratorController : FuelMachine, IInteractable {
 
-    public GameObject coalGeneratorInterfacePrefab;
     public GameObject outputNode;
 
-    private Container fuelContainer = new Container();
-    private CoalGeneratorUIController generatorUI;
-
-    private FuelItem fuelItem;
-    private FuelItem currentFuelItem;
-    private float remainingBurnTime;
     private float generatedPowerPerTick = 5f;
 
     // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
-        fuelContainer.Init(1);
-
-        remainingBurnTime = 0;
-        fuelItem = fuelContainer.savedSlots[0].item as FuelItem;
-        currentFuelItem = fuelItem;
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        if (remainingBurnTime < 0) {
-            remainingBurnTime = 0;
-        }
-        if (remainingBurnTime == 0 && fuelItem != null) {
-            currentFuelItem = fuelItem;
-            remainingBurnTime += currentFuelItem.burnTicks;
-            fuelContainer.SubtractItem(fuelItem, 1);
+        base.Update();
+
+        if (NeedsFuel()) {
+            ConsumeFuel();
         }
 
-        if (remainingBurnTime == 0) {
-            if (generatorUI != null) {
-                generatorUI.ShowInactive();
-            }
-            outputNode.GetComponent<WireNode>().powerContributionPerTick = 0;
-        }
-        else {
-            remainingBurnTime -= Time.deltaTime;
-            if (generatorUI != null) {
-                generatorUI.ShowActive();
-            }
+        if (HasActiveFuel()) {
             outputNode.GetComponent<WireNode>().powerContributionPerTick = generatedPowerPerTick;
-        }
-
-        if (generatorUI != null && currentFuelItem != null) {
-            generatorUI.UpdateBurnTimer(remainingBurnTime / currentFuelItem.burnTicks);
+        } else {
+            outputNode.GetComponent<WireNode>().powerContributionPerTick = 0;
         }
     }
 
@@ -61,28 +35,6 @@ public class CoalGeneratorController : MonoBehaviour, IInteractable {
     }
 
     public void RightClickInteract() {
-        OpenInterface();
+        ConnectToUI();
     }
-
-    public void OpenInterface() {
-        InventoryController.instance.OpenInterface(coalGeneratorInterfacePrefab);
-        InventoryController.instance.inventoryContainer.slotHolder = InventoryController.instance._interface.transform.GetChild(0).Find("InventorySlotHolder").gameObject;
-        InventoryController.instance.inventoryContainer.Reinit(InventoryController.instance.inventoryContainer.savedSlots);
-        InventoryController.instance.hotbarContainer.slotHolder = InventoryController.instance._interface.transform.GetChild(0).Find("HotbarSlotHolder").gameObject;
-        InventoryController.instance.hotbarContainer.Reinit(InventoryController.instance.hotbarContainer.savedSlots);
-        fuelContainer.slotHolder = InventoryController.instance._interface.transform.GetChild(0).Find("FuelSlotHolder").gameObject;
-        fuelContainer.Reinit();
-
-        generatorUI = InventoryController.instance._interface.GetComponent<CoalGeneratorUIController>();
-
-        fuelContainer.savedSlots[0].ItemUpdate += () => UpdateItems();
-
-        float burnPercent = currentFuelItem != null ? remainingBurnTime / currentFuelItem.burnTicks : 0;
-        generatorUI.UpdateBurnTimer(burnPercent);
-    }
-
-    public void UpdateItems() {
-        fuelItem = fuelContainer.savedSlots[0].item as FuelItem;
-    }
-
 }
