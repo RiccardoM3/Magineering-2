067b2e1c80b89e8e2be9618ce55abb5fcc54b544
Riccardo <ric.menon2000@gmail.com>
2020-10-05 11:42:29 +1100

    about to rework recipes

--- Assets/Scripts/Item/Recipe.cs
+++ Assets/Scripts/Item/Recipe.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public abstract class Recipe : ScriptableObject
 {
+
+    public enum RecipeType {
+        Crafting,
+        Smelting,
+        Crushing,
+        PlateMaking,
+        RodMaking,
+        WireMaking
+    }
+
     public Item item;
     public int amount;
     public List<SavedSlot> recipeItems = new List<SavedSlot>();
+    public RecipeType type;
 }
